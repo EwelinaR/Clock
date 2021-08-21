60366c6907113e625906d5569e5bd8e83983632f
Ewelina <ewelina.ruc@gmail.com>
2021-08-21 13:52:23 +0200

    Set full screen and landscape orientation

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.clock
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initScreen()
+    }
+
+    private fun initScreen() {
+        // hide bars
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
+
+        // use landscape orientation
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
     }
 }
